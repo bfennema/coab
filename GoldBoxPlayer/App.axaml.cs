e286b62eef1b652091b97893a9ff5d0141899c6f
Ben Fennema <benjamin.fennema@gmail.com>
2024-07-27 17:37:02 -0700

    Add new Avalonia based UI

---
+++ GoldBoxPlayer/App.axaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Threading;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Controls.Primitives;
+using Avalonia.Markup.Xaml;
+using GoldBoxPlayer.Services;
+using GoldBoxPlayer.ViewModels;
+using GoldBoxPlayer.Views;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GoldBoxPlayer;
+
+public partial class App : Application
+{
+    static Thread? engineThread;
+
+    public override void Initialize()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+
+    public override void OnFrameworkInitializationCompleted()
+    {
+        var services = new ServiceCollection();
+        MainViewModel model = null;
+        MainView view = null;
+        Classes.gbl.file = new File();
+
+        if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+        {
+            model = new MainViewModel();
+            desktop.MainWindow = new MainWindow
+            {
+                DataContext = model
+            };
+            services.AddSingleton<IFilesService>(x => new FilesService(desktop.MainWindow));
+            model.LoadConfigs(desktop.MainWindow);
+            view = desktop.MainWindow.GetControl<MainView>("MainView");
+            File.SetStorageProvider(desktop.MainWindow.StorageProvider);
+        }
+        else if (ApplicationLifetime is ISingleViewApplicationLifetime singleViewPlatform)
+        {
+            model = new MainViewModel();
+            singleViewPlatform.MainView = new MainView
+            {
+                DataContext = model
+            };
+            services.AddSingleton<IFilesService>(x => new FilesService(singleViewPlatform.MainView));
+            model.LoadConfigs(TopLevel.GetTopLevel(singleViewPlatform.MainView));
+            view = (MainView)singleViewPlatform.MainView;
+            File.SetStorageProvider(TopLevel.GetTopLevel(singleViewPlatform.MainView).StorageProvider);
+        }
+        else
+        {
+            return;
+        }
+
+        model.SetImage(view.GetControl<Image>("MainViewImage"));
+        if (Logging.Config.GetGame() == Logging.Game.PoolOfRadiance)
+        {
+            view.GetControl<MenuItem>("PoolRadMenu").IsChecked = true;
+        }
+        else if (Logging.Config.GetGame() == Logging.Game.CurseOfTheAzureBonds)
+        {
+            view.GetControl<MenuItem>("CurseMenu").IsChecked = true;
+        }
+        else if (Logging.Config.GetGame() == Logging.Game.SecretOfTheSilverBlades)
+        {
+            view.GetControl<MenuItem>("SecretMenu").IsChecked = true;
+        }
+
+        Services = services.BuildServiceProvider();
+
+        base.OnFrameworkInitializationCompleted();
+
+        if (!Design.IsDesignMode)
+        {
+            engineThread = new Thread(() => EngineThread(model));
+            engineThread.Name = "Engine";
+            engineThread.Start();
+        }
+    }
+
+    public new static App? Current => Application.Current as App;
+
+    public IServiceProvider? Services { get; private set; }
+
+
+    static void EngineThread(MainViewModel model)
+    {
+        engine.seg001.__SystemInit(model.EngineStopped, "GoldBoxPlayer.Desktop.Properties.Resources");
+        engine.seg001.PROGRAM();
+
+        model.EngineStopped();
+    }
+}
---
+++ GoldBoxPlayer/Views/MainView.axaml.cs
@@ -0,0 +1,169 @@
+﻿using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+
+namespace GoldBoxPlayer.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+
+    protected override void OnLoaded(RoutedEventArgs e)
+    {
+        base.OnLoaded(e);
+
+        var insetsManager = TopLevel.GetTopLevel(this)?.InsetsManager;
+
+        if (insetsManager != null)
+        {
+            insetsManager.DisplayEdgeToEdge = true;
+            insetsManager.IsSystemBarVisible = false;
+        }
+
+        MainViewUserControl.Focus();
+    }
+
+    private void Menu_PointerPressed(object? sender, PointerPressedEventArgs e)
+    {
+        MenuItem menu = (MenuItem)sender;
+        if (menu.IsChecked == false)
+        {
+            menu.IsChecked = true;
+            if (menu.Command != null)
+            {
+                menu.Command.Execute(menu.CommandParameter);
+            }
+        }
+    }
+
+    private void MainViewImage_KeyDown(object? sender, KeyEventArgs e)
+    {
+         engine.seg049.AddKey(KeyToIBMKey(e.Key));
+    }
+
+    public static ushort KeyToIBMKey(Key key)
+    {
+        if (key >= Key.D0 && key <= Key.D9)
+        {
+            return (ushort)((key - Key.D0) + '0');
+        }
+        if (key >= Key.A && key <= Key.Z)
+        {
+            return (ushort)((key - Key.A) + 'a');
+        }
+
+        if (key == Key.Enter)
+        {
+            return 0x1C0D;
+        }
+
+        if (key == Key.Space)
+        {
+            return 0x20;
+        }
+
+        if (key == Key.Delete)
+        {
+            return 0x5300;
+        }
+
+        if (key == Key.Back)
+        {
+            return 0x08;
+        }
+
+        if (key == Key.Home || key == Key.NumPad7 || key == Key.OemOpenBrackets)
+        {
+            return 0x4700;
+        }
+
+        if (key == Key.Up || key == Key.NumPad8)
+        {
+            return 0x4800;
+        }
+
+        if (key == Key.PageUp || key == Key.NumPad9)
+        {
+            return 0x4900;
+        }
+
+        if (key == Key.Left || key == Key.NumPad4)
+        {
+            return 0x4B00;
+        }
+
+        if (key == Key.NumPad5)
+        {
+            return 0x4C00;
+        }
+
+        if (key == Key.Right || key == Key.NumPad6)
+        {
+            return 0x4D00;
+        }
+
+
+        if (key == Key.End || key == Key.NumPad1 || key == Key.OemCloseBrackets)
+        {
+            return 0x4F00;
+        }
+
+        if (key == Key.Down || key == Key.NumPad2)
+        {
+            return 0x5000;
+        }
+
+        if (key == Key.PageDown || key == Key.NumPad3)
+        {
+            return 0x5100;
+        }
+
+        if (key == Key.OemMinus)
+        {
+            return 0x2d00;
+        }
+
+        if (key == Key.Escape)
+        {
+            return 0x1b;
+        }
+
+        if (key == Key.OemComma)
+        {
+            return 0x2c;
+        }
+
+        if (key == Key.OemPeriod)
+        {
+            return 0x2e;
+        }
+
+        return 0x0020;
+    }
+
+    private void MainViewUserControl_SizeChanged(object? sender, SizeChangedEventArgs e)
+    {
+        if (e.WidthChanged || e.HeightChanged)
+        {
+            var size = e.NewSize;
+            double scale;
+            double scale_height = size.Height / 200;
+            double scale_width = size.Width / 320;
+            if (scale_height > scale_width)
+            {
+                scale = scale_width; 
+            }
+            else
+            {
+                scale = scale_height;
+            }
+            int height = (int)(200 * scale);
+            int width = (int)(320 * scale);
+            MainViewImage.Width = width;
+            MainViewImage.Height = height;
+        }
+    }
+}
