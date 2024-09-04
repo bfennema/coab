f9f36108e531dd33206921c647e4772f8805bde9
Ben Fennema <benjamin.fennema@gmail.com>
2024-09-03 23:17:49 -0700

    Add create to file.cs (2)

--- Classes/File.cs
+++ Classes/File.cs
@@ -8,6 +8,7 @@ namespace Classes
     {
         public IAsyncEnumerable<(string,Stream)> OpenAll(string path, string filter = "*");
         public Task<Stream?> Open(string path, string filename);
+        public Task<Stream?> Create(string path, string filename);
         public void Delete(string path, string filename);
         public Task<bool> Find(string path, string filename);
 
