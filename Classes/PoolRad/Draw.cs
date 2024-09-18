8c31075a0401c20356c983cf4c2ad5389d6db741
Ben Fennema <benjamin.fennema@gmail.com>
2024-09-17 19:02:44 -0700

    Add support for pool of radiance credits

--- Classes/PoolRad/Draw.cs
+++ Classes/PoolRad/Draw.cs
@@ -23,6 +23,9 @@
         readonly static byte[] outer_frame_vertical_portrait =
             { 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0 };
 
+        readonly static int[] outer_frame_vertical_credits  =
+            { 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0 };
+
         readonly static byte[] inner_frame_vertical /*unk_16F31*/ =
             { 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0 };
         readonly static byte[] combat_frame_vertical /*unk_16F4D*/ =
@@ -32,22 +35,18 @@
         {
             Display.UpdateStop();
 
-            gbl.draw8x8_clear_area(0x16, 0x26, 1, 1);
+            gbl.draw8x8_clear_area(23, 39, 0, 0);
 
-            for (int col_x = 0; col_x <= 0x27; col_x++)
+            for (int col_x = 0; col_x <= 39; col_x++)
             {
                 gbl.Put8x8Symbol(0, false, outer_frame_horizontal[col_x] + 0x114, 0, col_x);
+                gbl.Put8x8Symbol(0, false, outer_frame_horizontal[col_x] + 0x114, 23, col_x);
             }
 
-            for (int row_y = 0; row_y < 0x17; row_y++)
+            for (int row_y = 0; row_y <= 23; row_y++)
             {
                 gbl.Put8x8Symbol(0, false, outer_frame_vertical[row_y] + 0x114, row_y, 0);
                 gbl.Put8x8Symbol(0, false, outer_frame_vertical[row_y] + 0x114, row_y, 39);
-            }
-
-            for (int col_x = 0; col_x <= 0x27; col_x++)
-            {
-                gbl.Put8x8Symbol(0, false, outer_frame_horizontal[col_x] + 0x114, 23, col_x);
             }
 
             Display.UpdateStart();
@@ -57,42 +56,54 @@
         {
             Display.UpdateStop();
 
-            Frame_Outer();
+            gbl.draw8x8_clear_area(24, 39, 0, 0);
 
-            for (int col_x = 0; col_x <= 0x27; col_x++)
+            for (int col_x = 0; col_x <= 39; col_x++)
             {
-                gbl.Put8x8Symbol(0, false, outer_frame_horizontal[col_x] + 0x114, 3, col_x);
-                gbl.Put8x8Symbol(0, false, outer_frame_horizontal[col_x] + 0x114, 8, col_x);
+                gbl.Put8x8Symbol(0, false, outer_frame_horizontal[col_x] + 0x114, 0, col_x);
+                gbl.Put8x8Symbol(0, false, outer_frame_horizontal[col_x] + 0x114, 4, col_x);
+                gbl.Put8x8Symbol(0, false, outer_frame_horizontal[col_x] + 0x114, 24, col_x);
             }
 
-            gbl.displayString("scenario created by:", 0, 10, 1, 2);
-            gbl.displayString("tsr, inc.", 0, 0x0e, 2, 11);
-            gbl.displayString("and", 0, 0x0a, 5, 0x15);
-            gbl.displayString("ssi", 0, 0x0e, 5, 0x19);
-            gbl.displayString("jeff grubb", 0, 0x0b, 6, 0x0e);
-            gbl.displayString("george mac donald", 0x0, 0x0B, 0x7, 0x0B);
-            gbl.displayString("game created by:", 0x0, 0x0A, 0x9, 0x1);
-            gbl.displayString("ssi special projects", 0x0, 0x0E, 0x9, 0x12);
-            gbl.displayString("project leader:", 0x0, 0x0E, 0x0B, 0x2);
-            gbl.displayString("george mac donald", 0x0, 0x0B, 0x0C, 0x2);
-            gbl.displayString("programming:", 0x0, 0x0E, 0x0E, 0x2);
-            gbl.displayString("scot bayless", 0x0, 0x0B, 0x0F, 0x2);
-            gbl.displayString("russ brown", 0x0, 0x0B, 0x10, 0x2);
-            gbl.displayString("michael mancuso", 0x0, 0x0B, 0x11, 0x2);
-            gbl.displayString("development:", 0x0, 0x0E, 0x13, 0x2);
-            gbl.displayString("david shelley", 0x0, 0x0B, 0x14, 0x2);
-            gbl.displayString("michael mancuso", 0x0, 0x0B, 0x15, 0x2);
-            gbl.displayString("oran kangas", 0x0, 0x0B, 0x16, 0x2);
-            gbl.displayString("graphic arts:", 0x0, 0x0E, 0x0B, 0x16);
-            gbl.displayString("tom wahl", 0x0, 0x0B, 0x0C, 0x16);
-            gbl.displayString("fred butts", 0x0, 0x0B, 0x0D, 0x16);
-            gbl.displayString("darla marasco", 0x0, 0x0B, 0x0E, 0x16);
-            gbl.displayString("susan halbleib", 0x0, 0x0B, 0x0F, 0x16);
-            gbl.displayString("testing:", 0x0, 0x0E, 0x12, 0x16);
-            gbl.displayString("jim jennings", 0x0, 0x0B, 0x13, 0x16);
-            gbl.displayString("james kucera", 0x0, 0x0B, 0x14, 0x16);
-            gbl.displayString("rick white", 0x0, 0x0B, 0x15, 0x16);
-            gbl.displayString("robert daly", 0x0, 0x0B, 0x16, 0x16);
+            for (int row_y = 0; row_y <= 23; row_y++)
+            {
+                gbl.Put8x8Symbol(0, false, outer_frame_vertical_credits[row_y] + 0x114, row_y, 0);
+                gbl.Put8x8Symbol(0, false, outer_frame_vertical_credits[row_y] + 0x114, row_y, 39);
+            }
+
+            gbl.displayString("scenario created by:", 0, 10, 1, 5);
+            gbl.displayString("tsr, inc.", 0, 14, 1, 26);
+            gbl.displayString("jim ward", 0, 10, 2, 15);
+            gbl.displayString("david cook,steve winter,mike breault", 0, 10, 3, 2);
+            gbl.displayString("game created by:", 0, 10, 5, 1);
+            gbl.displayString("ssi special projects", 0, 14, 5, 18);
+            gbl.displayString("version 1.3", 0, 11, 6, 14);
+            gbl.displayString("programming:", 0, 14, 7, 2);
+            gbl.displayString("original", 0, 14, 7, 20);
+            gbl.displayString("scot bayless", 0, 11, 8, 2);
+            gbl.displayString("programming:", 0, 14, 8, 20);
+            gbl.displayString("brad byers", 0, 11, 9, 2);
+            gbl.displayString("keith brors", 0, 11, 9, 20);
+            gbl.displayString("russ brown", 0, 11, 10, 2);
+            gbl.displayString("brad myers", 0, 11, 10, 20);
+            gbl.displayString("ted greer", 0, 11, 11, 2);
+            gbl.displayString("graphic arts:", 0, 14, 13, 2);
+            gbl.displayString("encounter coding:", 0, 14, 13, 20);
+            gbl.displayString("tom wahl", 0, 11, 14, 2);
+            gbl.displayString("paul murray", 0, 11, 14, 20);
+            gbl.displayString("fred butts", 0, 11, 15, 2);
+            gbl.displayString("russ brown", 0, 11, 15, 20);
+            gbl.displayString("darla marasco", 0, 11, 16, 2);
+            gbl.displayString("victor penman", 0, 11, 16, 20);
+            gbl.displayString("susan halbleib", 0, 11, 17, 2);
+            gbl.displayString("dave shelley", 0, 11, 17, 20);
+            gbl.displayString("project manager:", 0, 14, 19, 2);
+            gbl.displayString("developer:", 0, 14, 19, 20);
+            gbl.displayString("victor penman", 0, 11, 20, 2);
+            gbl.displayString("george mac donald", 0, 11, 20, 20);
+            gbl.displayString("testing:", 0, 14, 22, 2);
+            gbl.displayString("joel billings,steve salyer", 0, 11, 22, 11);
+            gbl.displayString("james kucera,robert daly,rick white", 0, 11, 23, 2);
 
             Display.UpdateStart();
         }
@@ -102,12 +113,12 @@
 
             Frame_Outer();
 
-            for (int colX = 0; colX <= 0x27; colX++)
+            for (int colX = 0; colX <= 39; colX++)
             {
                 gbl.Put8x8Symbol(0, false, frame_horizontal[colX] + 0x114, 16, colX);
             }
 
-            for (int rowY = 0; rowY <= 0x10; rowY++)
+            for (int rowY = 0; rowY <= 16; rowY++)
             {
                 gbl.Put8x8Symbol(0, false, frame_middle_vertical[rowY] + 0x114, rowY, 16);
             }
@@ -148,26 +159,26 @@
         {
             Display.UpdateStop();
 
-            gbl.draw8x8_clear_area(0x10, 0x26, 1, 1);
+            gbl.draw8x8_clear_area(16, 38, 0, 0);
 
-            for (int col_x = 0; col_x <= 0x27; col_x++)
+            for (int col_x = 0; col_x <= 39; col_x++)
             {
                 gbl.Put8x8Symbol(0, false, outer_frame_horizontal[col_x] + 0x114, 0, col_x);
             }
 
-            for (int row_y = 0; row_y <= 0x17; row_y++)
+            for (int row_y = 0; row_y <= 23; row_y++)
             {
                 gbl.Put8x8Symbol(0, false, outer_frame_vertical[row_y] + 0x114, row_y, 0);
                 gbl.Put8x8Symbol(0, false, outer_frame_vertical[row_y] + 0x114, row_y, 39);
             }
 
-            for (int col_x = 0; col_x <= 0x27; col_x++)
+            for (int col_x = 0; col_x <= 39; col_x++)
             {
                 gbl.Put8x8Symbol(0, false, outer_frame_horizontal[col_x] + 0x114, 2, col_x);
                 gbl.Put8x8Symbol(0, false, outer_frame_horizontal[col_x] + 0x114, 16, col_x);
             }
 
-            for (int col_x = 0; col_x <= 0x27; col_x++)
+            for (int col_x = 0; col_x <= 39; col_x++)
             {
                 gbl.Put8x8Symbol(0, false, outer_frame_horizontal[col_x] + 0x114, 23, col_x);
             }
