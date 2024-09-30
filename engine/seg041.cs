abb4ca04ebc8cc87255056c65920b7c3fe15caeb
Ben Fennema <benjamin.fennema@gmail.com>
2024-09-29 21:32:13 -0700

    Increase GameDelay minimum delay

--- engine/seg041.cs
+++ engine/seg041.cs
@@ -336,7 +336,7 @@ namespace engine
         internal static void GameDelay()
         {
             Display.ForceUpdate();
-            seg049.SysDelay(System.Math.Max(gbl.game_speed_var * 100, 50));
+            seg049.SysDelay(System.Math.Max(gbl.game_speed_var * 100, 400));
         }
     }
 }
