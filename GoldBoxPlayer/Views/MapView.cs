91b27696713155b2f08adf4d8b72ea3ba94235e3
Ben Fennema <benjamin.fennema@gmail.com>
2026-06-15 20:29:31 -0700

    Map Window #3

--- GoldBoxPlayer/Views/MapView.cs
+++ GoldBoxPlayer/Views/MapView.cs
@@ -105,7 +105,7 @@ namespace GoldBoxPlayer.Views
                         bool neighborExplored = (y > 0) && MapTracker.IsExplored(gameArea, blockId, x, y - 1);
                         if (currentExplored || neighborExplored)
                         {
-                            DrawBoundary(context, cellLeft, cellTop, cellRight, cellTop, mi.x3_dir_0, cellWidth, cellHeight, true, currentExplored, neighborExplored);
+                            DrawBoundary(context, cellLeft, cellTop, cellRight, cellTop, mi.x3_dir_0, cellWidth, cellHeight, true, neighborExplored, currentExplored);
                         }
                     }
                     // East boundary: cell (y, x) [Side A] vs cell (y, x + 1) [Side B]
@@ -114,7 +114,7 @@ namespace GoldBoxPlayer.Views
                         bool neighborExplored = (x < mapCols - 1) && MapTracker.IsExplored(gameArea, blockId, x + 1, y);
                         if (currentExplored || neighborExplored)
                         {
-                            DrawBoundary(context, cellRight, cellTop, cellRight, cellBottom, mi.x3_dir_2, cellWidth, cellHeight, false, currentExplored, neighborExplored);
+                            DrawBoundary(context, cellRight, cellTop, cellRight, cellBottom, mi.x3_dir_2, cellWidth, cellHeight, false, neighborExplored, currentExplored);
                         }
                     }
                     // South boundary: cell (y, x) [Side A] vs cell (y + 1, x) [Side B]
@@ -245,7 +245,7 @@ namespace GoldBoxPlayer.Views
         {
             var wallPen = new Pen(new SolidColorBrush(Color.Parse("#4a371c")), 3.5);
             var doorBrush = new SolidColorBrush(Colors.White);
-            var doorPen = new Pen(new SolidColorBrush(Colors.Black), 1.0);
+            var doorPen = new Pen(new SolidColorBrush(Color.Parse("#4a371c")), 1.0);
             var lockedDoorBrush = new SolidColorBrush(Color.Parse("#e65100")); // Orange
             var secretDoorPen = new Pen(new SolidColorBrush(Color.Parse("#b71c1c")), 2.0, dashStyle: DashStyle.Dash); // Dashed red
 
@@ -260,112 +260,53 @@ namespace GoldBoxPlayer.Views
                 double midX = (x1 + x2) / 2;
                 double midY = (y1 + y2) / 2;
 
-                double doorWidth = isHorizontal ? cellWidth * 0.35 : cellWidth * 0.12;
-                double doorHeight = isHorizontal ? cellHeight * 0.12 : cellHeight * 0.35;
+                double doorWidth = isHorizontal ? cellWidth * 0.45 : cellWidth * 0.24;
+                double doorHeight = isHorizontal ? cellHeight * 0.24 : cellHeight * 0.45;
 
                 var brush = (doorFlag == 2) ? lockedDoorBrush : doorBrush;
 
                 if (isHorizontal)
                 {
                     // Side A is below (positive Y), Side B is above (negative Y)
-                    if (sideAExplored)
+                    if (sideAExplored && sideBExplored)
                     {
-                        // Draw bottom half
-                        Rect r = new Rect(midX - doorWidth / 2, midY, doorWidth, doorHeight / 2);
+                        // Draw top and bottom half
+                        Rect r = new Rect(midX - doorWidth / 2, midY - doorHeight / 2, doorWidth, doorHeight);
                         context.DrawRectangle(brush, doorPen, r);
                     }
-                    if (sideBExplored)
+                    else if (sideAExplored)
                     {
                         // Draw top half
-                        Rect r = new Rect(midX - doorWidth / 2, midY - doorHeight / 2, doorWidth, doorHeight / 2);
+                        Rect r = new Rect(midX - doorWidth / 2, midY - doorHeight / 2, doorWidth, (doorHeight - 3.5) / 2);
+                        context.DrawRectangle(brush, doorPen, r);
+                    }
+                    else if (sideBExplored)
+                    {
+                        // Draw bottom half
+                        Rect r = new Rect(midX - doorWidth / 2, midY + (3.5 / 2), doorWidth, (doorHeight - 3.5) / 2);
                         context.DrawRectangle(brush, doorPen, r);
                     }
                 }
                 else
                 {
-                    // Vertical boundary.
-                    // For East boundary, Side A is left (negative X), Side B is right (positive X).
-                    // For West boundary, Side A is right (positive X), Side B is left (negative X).
-                    // To be general, we can look at the relative position of the cell coordinate or just use x1, y1.
-                    // Since it's vertical, we can draw the left half if either (East boundary and sideAExplored) or (West boundary and sideBExplored).
-                    // To keep it simple: we can draw left half if the left cell is explored, and right half if the right cell is explored!
-                    // Let's determine which side is left and right of this vertical line:
-                    // The line is at midX. The cell to the left is x - 1, the cell to the right is x.
-                    // So if sideAExplored corresponds to cell (y, x) and sideBExplored to (y, x - 1) [for West boundary],
-                    // then cell to the right is Side A, cell to the left is Side B.
-                    // If Side A is on the right, we draw right half if sideAExplored, and left half if sideBExplored.
-                    // If Side A is on the left, we draw left half if sideAExplored, and right half if sideBExplored.
-                    // Since East boundary: Side A is left (x), Side B is right (x+1). So Side A is left.
-                    // Since West boundary: Side A is right (x), Side B is left (x-1). So Side A is right.
-
-                    // Let's check which boundary it is:
-                    // If the boundary is at x1 (which is cellLeft or cellRight):
-                    // Let's just find out if the left cell of midX is explored, and right cell of midX is explored.
-                    // We can check:
-                    // Is the left cell visited? (xCoord = (int)(midX / cellWidth) - 1 if midX is on boundary)
-                    // It's much simpler:
-                    // For vertical boundary, Side A is either Left (for East boundary) or Right (for West boundary).
-                    // But wait, the loop in Render draws boundaries for each cell:
-                    // - East boundary of (y, x) is at cellRight. Left cell is current (Side A), Right cell is neighbor (Side B).
-                    //   So Left = sideAExplored, Right = sideBExplored.
-                    // - West boundary of (y, x) is at cellLeft. Right cell is current (Side A), Left cell is neighbor (Side B).
-                    //   So Right = sideAExplored, Left = sideBExplored.
-                    // But wait! Since we iterate over all cells, we will draw the East boundary of cell (y, x) and also the West boundary of cell (y, x+1).
-                    // Wait, do we double-draw boundaries?
-                    // Ah! The original code drew them for every cell, which could double-draw shared boundaries.
-                    // To draw them accurately:
-                    // If we just check:
-                    // Is the cell to the left of midX explored?
-                    // Let's compute left cell coordinate: `int leftX = (int)((midX - 0.1) / cellWidth);`
-                    // Is the cell to the right of midX explored?
-                    // Let's compute right cell coordinate: `int rightX = (int)((midX + 0.1) / cellWidth);`
-                    // This is extremely robust and doesn't depend on Side A/B mapping!
-                    // Let's do the same for horizontal boundary:
-                    // Is the cell above midY explored? `int topY = (int)((midY - 0.1) / cellHeight);`
-                    // Is the cell below midY explored? `int bottomY = (int)((midY + 0.1) / cellHeight);`
-
-                    int mapRows = gbl.geo_ptr.maps.GetLength(0);
-                    int mapCols = gbl.geo_ptr.maps.GetLength(1);
-                    int gameArea = gbl.game_area;
-                    int blockId = gbl.area_ptr != null ? gbl.area_ptr.current_3DMap_block_id : 0;
-
-                    if (isHorizontal)
+                    // Size A is to the right (positive X), Size B is to the left (negative X)
+                    if (sideAExplored && sideBExplored)
                     {
-                        int topY = (int)Math.Floor((midY - cellHeight * 0.1) / cellHeight);
-                        int bottomY = (int)Math.Floor((midY + cellHeight * 0.1) / cellHeight);
-
-                        bool topExplored = (topY >= 0 && topY < mapRows) && MapTracker.IsExplored(gameArea, blockId, (int)Math.Floor(midX / cellWidth), topY);
-                        bool bottomExplored = (bottomY >= 0 && bottomY < mapRows) && MapTracker.IsExplored(gameArea, blockId, (int)Math.Floor(midX / cellWidth), bottomY);
-
-                        if (bottomExplored)
-                        {
-                            Rect r = new Rect(midX - doorWidth / 2, midY, doorWidth, doorHeight / 2);
-                            context.DrawRectangle(brush, doorPen, r);
-                        }
-                        if (topExplored)
-                        {
-                            Rect r = new Rect(midX - doorWidth / 2, midY - doorHeight / 2, doorWidth, doorHeight / 2);
-                            context.DrawRectangle(brush, doorPen, r);
-                        }
+                        // Draw left and right half
+                        Rect r = new Rect(midX - doorWidth / 2, midY - doorHeight / 2, doorWidth, doorHeight);
+                        context.DrawRectangle(brush, doorPen, r);
                     }
-                    else
+                    else if (sideAExplored)
                     {
-                        int leftX = (int)Math.Floor((midX - cellWidth * 0.1) / cellWidth);
-                        int rightX = (int)Math.Floor((midX + cellWidth * 0.1) / cellWidth);
-
-                        bool leftExplored = (leftX >= 0 && leftX < mapCols) && MapTracker.IsExplored(gameArea, blockId, leftX, (int)Math.Floor(midY / cellHeight));
-                        bool rightExplored = (rightX >= 0 && rightX < mapCols) && MapTracker.IsExplored(gameArea, blockId, rightX, (int)Math.Floor(midY / cellHeight));
-
-                        if (leftExplored)
-                        {
-                            Rect r = new Rect(midX - doorWidth / 2, midY - doorHeight / 2, doorWidth / 2, doorHeight);
-                            context.DrawRectangle(brush, doorPen, r);
-                        }
-                        if (rightExplored)
-                        {
-                            Rect r = new Rect(midX, midY - doorHeight / 2, doorWidth / 2, doorHeight);
-                            context.DrawRectangle(brush, doorPen, r);
-                        }
+                        // Draw right half
+                        Rect r = new Rect(midX + (3.5/2), midY - doorHeight / 2, (doorWidth - 3.5)/ 2, doorHeight);
+                        context.DrawRectangle(brush, doorPen, r);
+                    }
+                    else if (sideBExplored)
+                    {
+                        // Draw left half
+                        Rect r = new Rect(midX - doorWidth / 2, midY - (doorHeight + 3.5)/ 2, (doorWidth - 3.5)/ 2, doorHeight);
+                        context.DrawRectangle(brush, doorPen, r);
                     }
                 }
             }
